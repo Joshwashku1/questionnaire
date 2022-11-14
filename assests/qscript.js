dfd17ffc0b209b11824ab3f74ed520ad9e2716f1
JoshuaHamann <joshwashku@yahoo.com>
2022-11-14 09:56:54 -0600

    added a timer countdown to html element

--- assests/qscript.js
+++ assests/qscript.js
@@ -1,11 +1,32 @@
 // Query selectors
 var startQuiz = document.querySelector(".start-quiz");
+var timerElement = document.querySelector(".timer-count");
+
+var timerCount;
 
 // Start quiz function: when this function is called upon it will start the quiz
 
 function sartQ() {
+    timerCount = 75;
+    timer();
 
 }
 
 // add event listener to call the function when click
 startQuiz.addEventListener("click", sartQ);
+
+// add a function to start a timer
+function timer(){
+
+    
+    start = setInterval(function(){
+        timerCount--;
+        timerElement.innerHTML = timerCount;
+        if(timerCount === 0){
+            clearInterval(start);
+            
+        }
+
+    }, 1000);
+     
+}
