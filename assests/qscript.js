e43ae7d2b0b5cd16ca50ac1b9ac7f992bf994837
JoshuaHamann <joshwashku@yahoo.com>
2022-11-14 09:38:29 -0600

    added a query in js to a start quiz button

---
+++ assests/qscript.js
@@ -0,0 +1,11 @@
+// Query selectors
+var startQuiz = document.querySelector(".start-quiz");
+
+// Start quiz function: when this function is called upon it will start the quiz
+
+function sartQ() {
+
+}
+
+// add event listener to call the function when click
+startQuiz.addEventListener("click", sartQ);
