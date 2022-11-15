5431ced04128a3c7e5107500a5affbf197645933
JoshuaHamann <joshwashku@yahoo.com>
2022-11-15 08:46:51 -0600

    added a list class in html for option buttons, displayed them inside script.js

--- assests/qscript.js
+++ assests/qscript.js
@@ -1,14 +1,26 @@
 // Query selectors
-var startQuiz = document.querySelector(".start-quiz");
+var startQuiz = document.querySelector(".start-quiz-btn");
 var timerElement = document.querySelector(".timer-count");
+var questionElement = document.querySelector(".questions");
+var optionList = document.querySelector(".q-options");
 
-var timerCount;
+var timerCount = 75;
+
+// initializes the page
+function init(){
+    optionList.style.display = "none";
+    timerElement.innerHTML = timerCount;
+}
 
 // Start quiz function: when this function is called upon it will start the quiz
 
 function sartQ() {
-    timerCount = 75;
-    timer();
+    
+
+    startQuiz.style.display = "none";
+    optionList.style.display = "block";
+    timerCountdown();
+    renderQ();
 
 }
 
@@ -16,17 +28,29 @@ function sartQ() {
 startQuiz.addEventListener("click", sartQ);
 
 // add a function to start a timer
-function timer(){
+function timerCountdown(){
 
     
-    start = setInterval(function(){
+    startTimer = setInterval(function(){
+
         timerCount--;
         timerElement.innerHTML = timerCount;
-        if(timerCount === 0){
-            clearInterval(start);
-            
+        if(timerCount > 0){
+
+        }else if(timerCount === 0){
+            clearInterval(startTimer);
+        
         }
 
     }, 1000);
      
 }
+
+// add a function to change through questions
+function renderQ(){
+    // var btn = document.createElement("button");
+    // btn.textContent = "HI";
+    // questionElement.appendChild(btn);
+}
+
+init();
