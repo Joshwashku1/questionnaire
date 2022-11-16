ead68e72e14a9ff88ad3f9f0752c355448304a1f
JoshuaHamann <joshwashku@yahoo.com>
2022-11-16 13:03:35 -0600

    added target event to option list elements

--- assests/qscript.js
+++ assests/qscript.js
@@ -3,33 +3,33 @@ var startQuiz = document.querySelector(".start-quiz-btn");
 var timerElement = document.querySelector(".timer-count");
 var questionElement = document.querySelector(".questions");
 var optionList = document.querySelector(".q-options");
+var qContainer = document.querySelector("q-container");
 
 
 var timerCount = 75;
 // var qNum = questionArray.length;
-var optAnswer;
-console.log(optAnswer);
+// qContainer.setAttribute("style", "display: block; border: dashed;");
 
 var questionArray = [
     {
     question: "What is an if statement?",
-    options: {
-        a: 'a loop',
-        b: 'a return',
-        c: 'a conditional statement',
-        d: 'a string',
-     },
+    options: [
+        'a loop',
+        'a return',
+        'a conditional statement',
+        'a string',
+    ],
     answer: 'a conditional statement'
     }, 
     {
     question: "The usual purpose of statement 1, in a for loop, is to initialize ____. "+ 
                 "[for(statement 1 ; statement2 ; statement 3 )]",
-    options:{
-        a: 'the count variable',
-        b: 'the length variable',
-        c: 'the object variable',
-        d: 'the mt. dew variable'
-    },
+    options:[
+        'the count variable',
+        'the length variable',
+        'the object variable',
+        'the mt. dew variable'
+    ],
     answer: 'the count variable'
     },
 ];
@@ -53,6 +53,11 @@ function getOptions(obj){
 }
 
 console.log(getOptions(questionArray));
+
+// function to check if the click is the answer
+function isAnswer(obj){
+
+}
 
 // initializes the page
 function init(){
@@ -98,20 +103,21 @@ function timerCountdown(){
 function renderQ(){
 
     optionList.style.display = "block";
-    questionElement.textContent = questionArray[0].question;
+    
     var optionArray = getOptions(questionArray);
 
     // for loop to render options
     for(i=0;i<1;i++){
+        questionElement.textContent = questionArray[i].question;
         var items = optionArray[i].length;
         console.log(items);
         console.log(optionArray)
         for(j=0;j<items;j++){
-        var optionItem = document.createElement('button');
+            var optionItem = document.createElement('button');
 
-        optionItem.innerHTML = optionArray[0][j];
-        console.log(optionArray[0][j]);
-        optionList.appendChild(optionItem);
+            optionItem.innerHTML = optionArray[0][j];
+            console.log(optionArray[0][j]);
+            optionList.appendChild(optionItem);
         }
     }
 
@@ -125,8 +131,13 @@ function renderQ(){
 // add an event listener to the option buttons
 optionList.addEventListener('click', function(event){
 
+    var target = event.target;
     //if clicked value is true = next question no time taken off
-    
+    if(target){
+    var yes = document.createElement('p')
+    alert(yes.textContent = 'yes');
+    }
+
     //if clicked value is false = next question time taken off
 });
 
